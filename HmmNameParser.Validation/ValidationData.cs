b5ff43c8bf3b972e9eed2082fab4b98849cd8960
rferg <rmferguson77@gmail.com>
2019-04-24 10:00:58 -0400

    some additional validation tests

--- HmmNameParser.Validation/ValidationData.cs
+++ HmmNameParser.Validation/ValidationData.cs
@@ -12,15 +12,107 @@ namespace HmmNameParser.Validation
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[]
+            List<object[]> data = new List<object[]>()
             {
-                "John Smith",
-                new Name
+                new object[]
                 {
-                    FirstName = "John",
-                    LastName = "Smith"
+                    "John Smith",
+                    new Name()
+                    {
+                        FirstName = "John",
+                        LastName = "Smith"
+                    }
+                },
+                new object[]
+                {
+                    "Mr. John Smith",
+                    new Name()
+                    {
+                        Prefix = "Mr.",
+                        FirstName = "John",
+                        LastName = "Smith"
+                    }
+                },
+                new object[]
+                {
+                    "Mr. John Smith, Jr.",
+                    new Name()
+                    {
+                        Prefix = "Mr.",
+                        FirstName = "John",
+                        LastName = "Smith",
+                        Suffix = "Jr."
+                    }
+                },
+                new object[]
+                {
+                    "John Smith, Jr.",
+                    new Name()
+                    {
+                        FirstName = "John",
+                        LastName = "Smith",
+                        Suffix = "Jr."
+                    }
+                },
+                new object[]
+                {
+                    "Lt. Col. John Smith, Jr. (Ret.)",
+                    new Name()
+                    {
+                        Prefix = "Lt. Col.",
+                        FirstName = "John",
+                        LastName = "Smith",
+                        Suffix = "Jr. (Ret.)"
+                    }
+                },
+                new object[]
+                {
+                    "John D. Smith, Jr.",
+                    new Name()
+                    {
+                        FirstName = "John",
+                        MiddleName = "D.",
+                        LastName = "Smith",
+                        Suffix = "Jr."
+                    }
+                },
+                new object[]
+                {
+                    "Rev. John D. Smith, Jr.",
+                    new Name()
+                    {
+                        Prefix = "Rev.",
+                        FirstName = "John",
+                        MiddleName = "D.",
+                        LastName = "Smith",
+                        Suffix = "Jr."
+                    }
+                },
+                new object[]
+                {
+                    "Karen Killinger-Humes",
+                    new Name()
+                    {
+                        FirstName = "Karen",
+                        LastName = "Killinger-Humes"
+                    }
+                },
+                new object[]
+                {
+                    "C. S. Lewis",
+                    new Name()
+                    {
+                        FirstName = "C.",
+                        MiddleName = "S.",
+                        LastName = "Lewis"
+                    }
                 }
             };
+
+            for (int i = 0; i < data.Count(); i++)
+            {
+                yield return data[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
